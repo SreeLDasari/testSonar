9b9a8cf53b34bff8a10522ee38c4b0c5f68785c6
SreeLDasari <73245619+SreeLDasari@users.noreply.github.com>
2021-06-02 11:06:00 +0530

    first commit

---
+++ src/test/java/Examples/Dummy.java
@@ -0,0 +1,16 @@
+package Examples;
+
+import org.testng.annotations.Test;
+
+public class Dummy {
+
+	@Test
+	public void t1() {
+		System.out.println("This is dummy");
+	}
+	@Test
+	public void t2() {
+		System.out.println("This is dummy");
+	}
+	
+}
